84753dafa5338206a4aab1b8d005cc22f260ea31
zhaogongchengsi <zzh1586169624@163.com>
2022-09-26 00:23:45 +0800

    add: app bg color

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-screen h-screen bg-white dark:bg-black">
+  <div class="w-screen h-screen app-bg">
     <TopMenu />
     <div class="main">
       <slot></slot>
@@ -13,5 +13,13 @@ import TopMenu from "./TopMenu.vue";
 .main {
   width: 100%;
   height: calc(100vh - var(--top-menu-height));
+}
+.application-container {
+background: rgba( 0, 0, 0, 0.9 );
+box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+backdrop-filter: blur( 18px );
+-webkit-backdrop-filter: blur( 18px );
+border-radius: 10px;
+border: 1px solid rgba( 255, 255, 255, 0.18 );
 }
 </style>
