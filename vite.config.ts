1714d22718864586b826b2ddd2d8f75ce531ba9b
zhaogongchengsi <zzh1586169624@163.com>
2022-09-25 16:09:03 +0800

    File proxy affects picture path

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,22 @@ import vue from '@vitejs/plugin-vue'
 import Unocss from "unocss/vite";
 import { presetAttributify, presetUno } from "unocss";
 import presetIcons from "@unocss/preset-icons";
+import { join, resolve } from "path"
+// import os from 'os'
 
-import { resolve } from "path"
-
-export default defineConfig({
-  plugins: [
-    vue(),
-    Unocss({
-      presets: [
-        presetAttributify(),
-        presetUno(),
-        presetIcons(),
-      ],
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": resolve("./src/"),
+export default defineConfig(() => {
+  return {
+    publicDir: join(process.env.HOME, "Pictures"),
+    plugins: [
+      vue(),
+      Unocss({
+        presets: [presetAttributify(), presetUno(), presetIcons()],
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": resolve("./src/"),
+      },
     },
-  },
+  };
 });
