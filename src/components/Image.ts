2db40b072a676ebe38b59de6bcafc65fc33c0f3c
zhaogongchengsi <zzh1586169624@163.com>
2022-10-03 18:33:18 +0800

    use lading to discard image lazy loading

--- src/components/Image.ts
+++ src/components/Image.ts
@@ -13,10 +13,11 @@ export default defineComponent({
     },
   },
   async setup(props) {
-    const vSrcDir = resolveDirective("src");
+    // const vSrcDir = resolveDirective("src");
     const src = await rendererImg(props.src);
-    const render = () => h("img", { src, alt: props.src });
-    if (vSrcDir) return () => withDirectives(render(), [[vSrcDir, props.src]]);
-    else return render;
+    const render = () => h("img", { src: src, alt: props.src });
+    // if (vSrcDir) return () => withDirectives(render(), [[vSrcDir, src]]);
+    // else return render;
+    return render;
   },
 });
